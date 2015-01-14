afc55505889473bdaad8238a73c9e6fbd6b7c3b6
David Zulaica & Marc Lee <student@epicodus.com>
2015-01-14 14:07:15 -0800

    add save method

--- lib/term.rb
+++ lib/term.rb
@@ -1,5 +1,7 @@
 class Term
 
+  @@terms = []
+
   define_method(:initialize) do |word,definition|
     @word = word
     @definition = definition
@@ -8,5 +10,19 @@ class Term
   define_method(:word) do
     @word
   end
+
+  define_method(:definition) do
+    @definition
+  end
+
+  define_method(:save) do
+    @@terms.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@terms
+  end
+
+
 
 end
