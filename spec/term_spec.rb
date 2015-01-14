a26392fc7d150041221606476abfb5771fa14567
David Zulaica & Marc Lee <student@epicodus.com>
2015-01-14 14:10:47 -0800

    Add tests for save and clear methods.

--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -2,9 +2,9 @@ require("rspec")
 require("term")
 
 describe("Term") do
-  # before() do
-  #   Term.clear()
-  # end
+  before() do
+    Term.clear()
+  end
 
   describe("#word") do
     it("returns the word in our term") do
@@ -13,4 +13,24 @@ describe("Term") do
     end
   end
 
+  describe("#definition") do
+    it("returns the definition") do
+      test_term = Term.new("carrot","A delicious vegetable.")
+      expect(test_term.definition()).to(eq("A delicious vegetable."))
+    end
+  end
+
+  describe("#save") do
+    it("adds a term to the terms array") do
+      test_term = Term.new("carrot","A delicious vegetable.")
+      test_term.save()
+      expect(Term.all()).to(eq([test_term]))
+    end
+  end
+
+  describe(".all") do
+    it("returns an empty list") do
+      expect(Term.all()).to(eq([]))
+    end
+  end
 end
