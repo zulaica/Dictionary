565e85f432ce78018d8bc5ba0d66449aac6f0350
David Zulaica & Marc Lee <student@epicodus.com>
2015-01-14 15:51:15 -0800

    Add destroy method test.

--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -43,4 +43,16 @@ describe("Term") do
       expect(Term.find(test_term.id())).to(eq(test_term))
     end
   end
+
+  describe("#destroy") do
+    it("destroys a term by its id number") do
+      test_term = Term.new("carrot","A delicious vegetable.")
+      test_term.save()
+      other_test_term = Term.new("potato","Boil 'em, mash 'em, stick 'em in a stew!")
+      other_test_term.save()
+      test_term.destroy()
+      expect(Term.all().length()).to(eq(1))
+    end
+  end
+
 end
