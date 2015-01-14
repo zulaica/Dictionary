eb275682d4229b6f9797a8587c7d7097d9b56b5e
David Zulaica & Marc Lee <student@epicodus.com>
2015-01-14 12:15:15 -0800

    Add our word method test.

--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -2,5 +2,15 @@ require("rspec")
 require("term")
 
 describe("Term") do
+  # before() do
+  #   Term.clear()
+  # end
+
+  describe("#word") do
+    it("returns the word in our term") do
+      test_term = Term.new("carrot","A delicious vegetable.")
+      expect(test_term.word()).to(eq("carrot"))
+    end
+  end
 
 end
